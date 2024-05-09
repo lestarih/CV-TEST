1f72aeefbbde45cf1e48df4e8a93fc94f4b1aba6
Lestari.Natalia <lestarinatalia@indomaret.co.id>
2024-05-09 17:36:18 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CV/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CV cv)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save CV details to database or perform any other actions
+                // For demonstration purposes, we'll just redirect to ShowCV action
+                return RedirectToAction("ShowCV", cv);
+            }
+
+            return View(cv);
+        }
+
+        // GET: CV/ShowCV
+        public ActionResult ShowCV(CV cv)
+        {
+            // Display CV details on ShowCV page
+            return View(cv);
+        }
+    }
+}
---
+++ Models/CV.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CV
+{
+    public class CV
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Experience { get; set; }
+
+        [Required]
+        public string Skills { get; set; }
+
+    }
+}
